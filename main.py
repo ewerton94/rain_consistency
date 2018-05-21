1965eed1dac44135217d18963f556193cb07b73b
Ewerton Amorim <ewerton.amorim@live.com>
2018-05-21 09:20:36 -0300

    Atualizado main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #imports do Ewerton:
-
+from reads_bin import get_data_from_radar
+from reads_txt import get_station_info_from_txt
+import pandas as pd
 
 #imports do Cayo:
 
@@ -13,6 +15,13 @@
 
 if __name__=="__main__":
     pass
+
+
+    #postos = get_station_info_from_txt('entrada.txt')
+    #data_from_radar = get_data_from_radar(postos)
+    #data_from_stations = []
+    #print(list(data_from_radar.values())[0])
+
 
 
 
