6099ff445c342bb755355abbb38f5fc08bff80bc
Ewerton Amorim <ewerton.amorim@live.com>
2018-05-23 00:19:45 -0300

    Atualizado main para compatibilidade e melhoras na organização

--- main.py
+++ main.py
@@ -1,41 +1,53 @@
-#imports do Ewerton:
+import pandas as pd
 from reads_bin import get_data_from_radar
 from reads_txt import get_station_info_from_txt, input_pluvio_data, get_events_dates
 from reads_excel import get_station_data, read_excel_cemaden
 from test_module import *
-import pandas as pd
-
-#imports do Cayo:
-
-
-# Funções do Ewerton:
-
-
-# Funções do Cayo:
-
+from metrics import metrics
+from uncertainty_analysis import mae, rmse, corr_coef
+from bootstrap import uncertainty_analysis
 
 
 if __name__=="__main__":
+    # Dados de entrada:
     stations_metadata_filename = 'entrada.txt'
-    #data_from_pluviometric_station = input_pluvio_data(stations_metadata_filename=stations_metadata_filename, stations_data_dir='txt/Evento 1/')
+    excel_files_dir = 'excel/'
+    events_filename = 'events.txt'
+    # Obtendo metadados das estações
     stations = get_station_info_from_txt(stations_metadata_filename)
-    stations_data = read_excel_cemaden('excel/')
+    # Obtendo as datas dos eventos
+    events = get_events_dates(events_filename)
+    # Obtendo dados do CEMADEN
+    stations_data = read_excel_cemaden(excel_files_dir)
     data_from_pluviometric_station = {}
     for station in stations:
         data_from_pluviometric_station[station] = get_station_data(stations_data, station).groupby(pd.Grouper(freq='H')).mean()
-        data_from_pluviometric_station[station].index = data_from_pluviometric_station[station].index.to_datetime()
-    data_from_radar = get_data_from_radar(stations)
-    events = get_events_dates('events.txt')
-
-    for event in events:
-        event_data = merge_datas_df(data_from_pluviometric_station, data_from_radar, event)
-        print(event_data)
-        #print(list(data.values()[0]))
-
+    # Obtendo dados a partir dos radares
+    data_from_radar, models = get_data_from_radar(stations)
+    # Mesclando dados das estações e radares
     events_data = {}
     for station in stations:
-        events_data[station] = merge_datas_df_event(data_from_pluviometric_station[station], data_from_radar[station], event)
-        print(events_data.head())
+        events_data[station] = merge_datas_df_event(data_from_pluviometric_station[station], data_from_radar[station], events)
+    # Análise de ocorrência:
+    detect_anal(events_data, models)
+    #Análise de incertezas:
+    metric_data = metrics(events_data)
+    for station in metric_data:
+        for metric in ['acum', 'max']:
+            real = [d['Data'] for d in metric_data[station][metric]]
+            models_metric = {}
+            for model in models:
+                models_metric[model] = [d[model] for d in metric_data[station][metric]]
+                print('\n\nMODEL: ', model)
+                print(models_metric[model])
+                print(real)
+                print(metric)
+                print(uncertainty_analysis(models_metric[model], metric))
+                print(corr_coef(real, models_metric[model]))
+                print(mae(real, models_metric[model]))
+                print(rmse(real, models_metric[model]))
+    
+
 
     '''
     #data_from_stations = []
